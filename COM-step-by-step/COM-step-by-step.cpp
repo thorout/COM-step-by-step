a4db91e58624e7cd1d03a63bd9dee3c64be334f6
thorout <thorout@gmail.com>
2018-07-23 15:38:10 +0300

    chapter 2. simple example of using interfaces

--- COM-step-by-step/COM-step-by-step.cpp
+++ COM-step-by-step/COM-step-by-step.cpp
@@ -2,10 +2,51 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <objbase.h>
+
+interface IX
+{
+	virtual void __stdcall Fx1() = 0;
+	virtual void __stdcall Fx2() = 0;
+};
+
+interface IY
+{
+	virtual void __stdcall Fy1() = 0;
+	virtual void __stdcall Fy2() = 0;
+};
+
+
+class CA : public IX, public IY
+{
+public:
+	//Реализация интерфейса IX
+	virtual void __stdcall Fx1() override { std::cout << "CA::Fx()" << std::endl;}
+	virtual void __stdcall Fx2() override { std::cout << "CA::Fx()" << std::endl;}
+
+	//Реализация интерфейса IY
+	virtual void __stdcall Fy1() override { std::cout << "CA::Fy()" << std::endl;}
+	virtual void __stdcall Fy2() override { std::cout << "CA::Fy()" << std::endl;}
+};
 
 
 int main()
 {
+	CA* pA = new CA;
+
+	IX* pIX = pA;
+
+	pIX->Fx1();
+	pIX->Fx2();
+
+	IY* pIY = pA;
+
+	pIY->Fy1();
+	pIY->Fy2();
+
+	delete pA;
+
     return 0;
 }
 
